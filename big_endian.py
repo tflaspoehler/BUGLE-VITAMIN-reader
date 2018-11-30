364dd9e063d445fdce4c1ada0c060d44053b8b79
tflaspoehler <tflaspoehler@gmail.com>
2018-11-30 11:43:16 -0500

    cleaned up some stuff

--- big_endian.py
+++
@@ -1,8 +0,0 @@
-
-with open("Bugle7T_littleEndian", "rb") as f:
-    byte = f.read(1)
-    while byte != "":
-        # Do stuff with byte.
-        byte = f.read(1)
-        print byte
-        raw_input()
